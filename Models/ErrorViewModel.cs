c06550738a6ec6dbab0c263275b451f538db5475
unknown <abhiramikrishnamurthy92@gmail.com>
2026-02-06 08:45:56 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureLearningTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
